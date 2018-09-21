e2a9f573a57659c0ff21738479562e8ef41805a2
randomsgs <brandenchan@icloud.com>
2018-09-21 18:37:48 +0200

    Model builds. Train loop being developed

---
+++ data_handling.py
@@ -0,0 +1,138 @@
+import random
+import numpy as np
+import tensorflow as tf
+
+PAD_CODE = 0
+START_TOKEN = "<START>"
+END_TOKEN = "<END>"
+
+class JointIterator:
+    def __init__(self, iter_x, iter_y, time_major=True, auto_reset=True):
+        self.iter_x = iter_x
+        self.iter_y = iter_y
+        assert iter_x.len == iter_y.len
+        self.len = iter_x.len
+        self.time_major = time_major
+        self.auto_reset = auto_reset
+
+    def next(self, num):
+        X_raw = self.iter_x.next(num)
+        Y_raw = self.iter_y.next(num)
+        if not X_raw and not Y_raw:
+            return None
+        X_lens = np.asarray([len(seq) for seq in X_raw])
+        Y_lens = np.asarray([len(seq) for seq in Y_raw]) - 1
+        Y = pad(Y_raw)
+        X = pad(X_raw)
+        Y_in = np.asarray(Y)[:, :-1]
+        Y_targ = np.asarray(Y)[:, 1:]
+        X = np.stack(X)
+
+
+        # Y_in = np.stack(Y_in)
+        # Y_targ = np.stack(Y_targ)
+
+        if self.time_major:
+            X = X.T
+            Y_in = Y_in.T
+            Y_targ = Y_targ.T
+
+        return X, Y_in, Y_targ, X_lens, Y_lens
+
+    def reset(self):
+        self.iter_x.reset()
+        self.iter_y.reset()
+
+        
+class DataIterator:
+    """ This class modifies a regular iterator so that
+    it will start iterating back at the beginning when it
+    has reached the end """
+    def __init__(self, data, auto_reset=True):
+        self.auto_reset = auto_reset
+        self.save = data
+        self.len = len(data)
+        self.iterator = iter(self.save)
+
+    def next(self, num):
+        ret = []
+        for _ in range(num):
+            try:
+                ret.append(next(self.iterator))
+            except StopIteration:
+                if self.auto_reset:
+                    self.reset()
+                    ret.append(next(self.iterator))
+                else:
+                    return ret
+        return ret
+
+    def reset(self):
+        self.iterator = iter(self.save)
+
+def joint_iterator_from_file(filename, auto_reset=True, time_major=True):
+    X, Y = load_cmu(filename)
+    x_iter = DataIterator(X, auto_reset=auto_reset)
+    y_iter = DataIterator(Y, auto_reset=auto_reset)
+    return JointIterator(x_iter, y_iter, time_major=time_major)
+
+def pad(seq_of_seq, pad_code=PAD_CODE):
+    lens = np.asarray([len(s) for s in seq_of_seq])
+    max_len = max(lens)
+    pads_required = max_len - lens
+    for i, s in enumerate(seq_of_seq):
+        for _ in range(pads_required[i]):
+            s.append(pad_code)
+    return seq_of_seq
+
+def load_cmu(filename, start_end=True, encoding=True):
+    file = open(filename, encoding="utf-8")
+    spell_to_code, _ = load_maps("data/chars")
+    arpa_to_code, _ = load_maps("data/arpabet")
+    spellings = []
+    phonetics = []
+    for l in file:
+        if not l[0].isalpha():
+            continue
+        cols = l[:-1].split()
+        if len(cols) < 2:
+            continue
+        spell = cols[0]
+        phon = cols[1:]
+        if start_end:
+            spell = add_tokens(spell)
+            phon = add_tokens(phon)      
+        spellings.append(spell)
+        phonetics.append(phon)
+    assert len(spellings) == len(phonetics)
+    return convert_list(spellings, spell_to_code), convert_list(phonetics, arpa_to_code)
+
+def add_tokens(seq, start=START_TOKEN, end=END_TOKEN):
+    ret = [start] + list(seq)
+    ret.append(end)
+    return ret
+
+def convert_list(list, map):
+    ret = []
+    for word in list:
+        ret.append(convert_word(word, map))
+    return ret
+
+def convert_word(word, map):
+    ret = []
+    for ch in word:
+        ret.append(map[ch])
+    return ret
+
+def load_maps(sym_file):
+    syms = load_symbols(sym_file)
+    return create_map(syms)
+
+def load_symbols(filename):
+    return [sym[:-1] for sym in open(filename)]
+
+def create_map(symbols):
+    sym_to_code = {sym : i for i, sym in enumerate(symbols)}
+    code_to_sym = {i : sym for i, sym in enumerate(symbols)}
+    return sym_to_code, code_to_sym
+
---
+++ model.py
@@ -0,0 +1,390 @@
+import os
+import shutil
+from pprint import pprint
+
+import tensorflow as tf
+from tensorflow.nn.rnn_cell import LSTMCell, LSTMStateTuple
+import numpy as np
+
+
+from data_handling import (convert_list, joint_iterator_from_file, load_maps,
+                           load_symbols, convert_word)
+
+
+class CharToPhonModel:
+    """ Class that handles training and inference of LSTM model """
+    def __init__(self,
+                data_dir="data/",
+                batch_size=3,
+                embed_dims=200,
+                hidden_dims=200,
+                bidir=True,
+                cell_class=LSTMCell,
+                max_gradient_norm=1,
+                learning_rate=0.0001,
+                save_dir="output_1/",
+                resume_dir=None,
+                n_batches=10000,
+                debug=True,
+                sample_size=20,
+                print_every=50,
+                validate_every=200
+                ):
+
+        self.data_dir = data_dir
+        self.batch_size = batch_size
+        self.n_chars = len(load_symbols(data_dir + "chars"))
+        self.n_arpa = len(load_symbols(data_dir + "arpabet"))
+        _, self.code_to_chars = load_maps(data_dir + "chars")
+        _, self.code_to_arpa = load_maps(data_dir + "arpabet")
+        self.embed_dims = embed_dims
+        self.hidden_dims = hidden_dims
+        self.bidir = bidir
+        self.cell_class=LSTMCell
+        self.max_gradient_norm = max_gradient_norm
+        self.learning_rate = learning_rate
+        self.save_dir = save_dir
+        self.resume_dir = resume_dir
+        self.n_batches = n_batches
+        self.debug = debug
+        self.sample_size = sample_size
+        self.print_every = print_every
+        self.validate_every = validate_every
+        self.save_every = validate_every
+
+
+    def build_graph(self):
+        # tf.reset_default_graph()
+        self.setup_variables()
+        self.build_encoder()
+        self.build_decoder()
+        self.compute_loss()
+        self.gradient_update()
+
+
+    def setup_variables(self):
+        with tf.variable_scope("variables"):
+
+            # inputs of shape [encoder_max_time, batch_size]
+            self.encoder_inputs = tf.placeholder(shape=(None, None),
+                                                dtype=tf.int32,
+                                                name="encoder_inputs")
+            self.encoder_inputs = tf.Print(self.encoder_inputs, [self.encoder_inputs])
+
+            # inputs of shape [decoder_max_time, batch_size]
+            self.decoder_inputs = tf.placeholder(shape=(None, None),
+                                                dtype=tf.int32,
+                                                name="decoder_inputs")
+
+            # targets of shape [decoder_max_time - 1, batch_size]
+            self.decoder_targets = tf.placeholder(shape=(None, None),
+                                                dtype=tf.int32,
+                                                name="decoder_targets")
+
+            # sequence lengths of shape [batch_size]
+            self.encoder_input_lengths = tf.placeholder(shape=(None,),
+                                                        dtype=tf.int32,
+                                                        name='encoder_inputs_lengths')
+
+            # sequence lengths of shape [batch_size]
+            self.decoder_input_lengths = tf.placeholder(shape=(None,),
+                                                        dtype=tf.int32,
+                                                        name='decoder_input_lengths')
+
+            # sequence lengths of shape [batch_size]
+            self.decoder_target_lengths = tf.placeholder(shape=(None,),
+                                                        dtype=tf.int32,
+                                                        name='decoder_target_lengths')
+
+            self.char_embeddings = tf.Variable(tf.random_uniform((self.n_chars, self.embed_dims), -1.0, 1.0),
+                                            name="char_embeddings")
+
+            self.arpa_embeddings = tf.Variable(tf.random_uniform((self.n_arpa, self.embed_dims), -1.0, 1.0),
+                                            name="arpa_embeddings")
+
+            self.encoder_input_embeddings = tf.nn.embedding_lookup(self.char_embeddings, self.encoder_inputs)
+
+            self.decoder_input_embeddings = tf.nn.embedding_lookup(self.arpa_embeddings, self.decoder_inputs)
+
+            self.decoder_target_embeddings = tf.nn.embedding_lookup(self.arpa_embeddings, self.decoder_targets)
+
+    def build_encoder(self):
+        with tf.variable_scope("encoder"):
+
+            # Unidirectional Run
+            if not self.bidir:
+                encoder_cell = self.cell_class(self.hidden_dims)
+                encoder_outputs, self.encoder_final_state = tf.nn.dynamic_rnn(
+                                            encoder_cell, self.encoder_input_embeddings,
+                                            dtype=tf.float32, time_major=True)
+                del encoder_outputs
+
+            # Bidirectional Run
+            else:
+                with tf.variable_scope("fw"):
+                    fw_encoder_cell = self.cell_class(self.hidden_dims)
+                with tf.variable_scope("bw"):
+                    bw_encoder_cell = self.cell_class(self.hidden_dims)
+
+                ((encoder_fw_outputs, encoder_bw_outputs),
+                (encoder_fw_final_state, encoder_bw_final_state)) = (tf.nn.bidirectional_dynamic_rnn(cell_fw=fw_encoder_cell,
+                                                                                                    cell_bw=bw_encoder_cell,
+                                                                                                    inputs=self.encoder_input_embeddings,
+                                                                                                    sequence_length=self.encoder_input_lengths,
+                                                                                                    dtype=tf.float32, time_major=True))
+
+                # Concat final states of forward and backward run
+                encoder_final_state_c = tf.concat(
+                    (encoder_fw_final_state.c, encoder_bw_final_state.c), 1)
+
+                encoder_final_state_h = tf.concat(
+                    (encoder_fw_final_state.h, encoder_bw_final_state.h), 1)
+
+                self.encoder_final_state = LSTMStateTuple(
+                    c=encoder_final_state_c,
+                    h=encoder_final_state_h
+                )
+
+                del encoder_fw_outputs
+                del encoder_bw_outputs
+
+    def build_decoder(self):
+        with tf.variable_scope("decoder"):
+
+            with tf.variable_scope("projection"):
+                self.projection_layer = tf.layers.Dense(
+                                            self.n_arpa, use_bias=False)
+
+            decoder_dims = self.hidden_dims
+            if self.bidir:
+                decoder_dims *= 2
+            decoder_cell = self.cell_class(decoder_dims)
+
+            helper = tf.contrib.seq2seq.TrainingHelper(
+                            inputs=self.decoder_input_embeddings, 
+                            sequence_length=self.decoder_input_lengths,
+                            time_major=True)
+
+            my_decoder = tf.contrib.seq2seq.BasicDecoder(
+                            decoder_cell,
+                            helper,
+                            self.encoder_final_state,
+                            output_layer=self.projection_layer)
+
+            outputs, _, _ = tf.contrib.seq2seq.dynamic_decode(
+                                                    my_decoder,
+                                                    output_time_major=True,
+                                                    impute_finished=True)
+
+            self.logits = outputs.rnn_output
+            self.predictions_arpa = tf.transpose(tf.argmax(self.logits, 2))
+
+    def compute_loss(self):
+        with tf.variable_scope("loss_computation"):
+            self.crossent = tf.nn.sparse_softmax_cross_entropy_with_logits(
+                                        labels=self.decoder_targets, logits=self.logits)
+
+            self.sequence_mask = tf.sequence_mask(
+                                    self.decoder_target_lengths, name="sequence_mask", dtype=tf.float32)
+            self.sequence_mask = tf.transpose(self.sequence_mask)
+
+            self.num_predictions = tf.count_nonzero(self.sequence_mask, dtype=tf.float32)
+
+            self.train_loss = (tf.reduce_sum(self.crossent * self.sequence_mask) /
+                                self.num_predictions)
+
+    def gradient_update(self):
+        # Calculate and clip gradients
+        params = tf.trainable_variables()
+        gradients = tf.gradients(self.train_loss, params)
+        clipped_gradients, _ = tf.clip_by_global_norm(
+                                gradients, self.max_gradient_norm)
+
+        # Optimization
+        optimizer = tf.train.AdamOptimizer(self.learning_rate)
+        self.train_op = optimizer.apply_gradients(zip(clipped_gradients, params))
+
+
+    def train(self):
+        # Load data
+        train_file = self.data_dir + "train"
+        dev_file = self.data_dir + "dev"
+        if self.debug:
+            train_file += "_debug"
+            dev_file += "_debug"
+        self.iter_train = joint_iterator_from_file(train_file, auto_reset=True)
+        self.iter_dev = joint_iterator_from_file(dev_file, auto_reset=False)
+        self.n_train_data = self.iter_train.len
+
+        # Sample of data
+        self.sample = self.iter_train.next(self.sample_size)
+        print_sample(self.sample, self.code_to_chars, self.code_to_arpa)
+
+        n_to_process = self.n_batches * self.batch_size
+        print("\nTRAINING\n" + "="*20)
+        if self.bidir:
+            print("\tBidirectional encoder")
+        else:
+            print("\tUnidirectional encoder")
+        if self.debug:
+            print("\DEBUG MODE")
+        print("\tTraining {} examples over {} batches of {} ({} epochs)\n".format(self.n_train_data,
+                                                                              self.n_batches,
+                                                                              self.batch_size,
+                                                                              int(n_to_process / self.n_train_data)))
+
+
+        with tf.Session() as sess:
+            # Setup of output directory
+            check_save_dir(self.save_dir, self.resume_dir)
+            save_hyperparams(self)
+
+            # Build model and initialize variables
+            self.build_graph()
+
+            self.saver = tf.train.Saver()
+            if self.resume_dir:
+                self.saver.restore(sess, self.resume_dir + "model.ckpt")
+            else:
+                sess.run(tf.global_variables_initializer())
+
+            self.train_loop(sess)
+
+
+    def train_loop(self, sess):
+        completed_batches = 0
+        train_loss_track = []
+        dev_loss_track = []
+
+        for completed_batches in range(self.n_batches):
+
+            # Get batch of data and perform training
+            X, Y_in, Y_targ, X_lens, Y_lens = self.iter_train.next(self.batch_size)
+            # X, Y_in, Y_targ, X_lens, Y_lens = self.sample
+
+            fd = {self.encoder_inputs: X,
+                    self.decoder_inputs: Y_in,
+                    self.decoder_targets: Y_targ,
+                    self.encoder_input_lengths: X_lens,
+                    self.decoder_input_lengths: Y_lens,
+                    self.decoder_target_lengths: Y_lens}
+
+            _, loss, prediction = sess.run([self.train_op, self.train_loss, self.predictions_arpa], fd)
+            train_loss_track.append(loss)
+
+            # Printing, validation and saving
+            if completed_batches != 0:
+                epoch = (self.batch_size * completed_batches) // self.n_train_data
+                if completed_batches % self.validate_every == 0:
+                    dev_loss = self.validation(sess)
+                    dev_loss_track.append(dev_loss)
+                    print("Batch {} / {} Epoch {} dev: {}".format(completed_batches, self.n_batches, epoch, dev_loss))
+                if completed_batches % self.print_every == 0:
+                    t_loss = np.mean(train_loss_track[-100:])
+                    print("Batch {} / {} Epoch {} train: {}".format(completed_batches, self.n_batches, epoch, t_loss))
+                if completed_batches % self.save_every == 0:
+                    save_path = self.saver.save(sess, self.save_dir + "model.ckpt")
+                    print("Model saved in path: {}\n".format(save_path))
+
+
+
+            # every X amount of batches: save train and dev loss, save check point, do dev
+
+    def validation(self, sess):
+        print()
+        self.sample_inference(sess)
+        print()
+        dev_loss = self.dev_validation(sess)
+        return dev_loss
+
+    def sample_inference(self, sess):
+        X, Y_in, Y_targ, X_lens, Y_lens = self.sample
+        fd = {self.encoder_inputs: X,
+                self.decoder_inputs: Y_in,
+                self.decoder_targets: Y_targ,
+                self.encoder_input_lengths: X_lens,
+                self.decoder_input_lengths: Y_lens,
+                self.decoder_target_lengths: Y_lens}
+        prediction, = sess.run([self.predictions_arpa], fd)
+        print_prediction(X, prediction, self.code_to_chars, self.code_to_arpa)
+        evaluate(Y_targ, prediction)
+
+
+    def dev_validation(self, sess):
+        self.iter_dev.reset()
+        all_losses = []
+        while True:
+            batch = self.iter_dev.next(self.batch_size)
+            
+            if not batch:
+                # i.e. at the end of the dev epoch
+                break
+
+            X, Y_in, Y_targ, X_lens, Y_lens = batch
+
+            fd = {self.encoder_inputs: X,
+                    self.decoder_inputs: Y_in,
+                    self.decoder_targets: Y_targ,
+                    self.encoder_input_lengths: X_lens,
+                    self.decoder_input_lengths: Y_lens,
+                    self.decoder_target_lengths: Y_lens}
+            loss, = sess.run([self.train_loss], fd)
+            all_losses.append(loss)
+        return np.mean(all_losses)
+
+
+
+
+def save_hyperparams(model, filename="hyperparameters.txt"):
+    hyperparams = vars(model)
+    with open(model.save_dir + filename, "w") as output:
+        for hp in hyperparams:
+            output.write("{}: {}\n".format(hp, hyperparams[hp]))
+
+def check_save_dir(save_dir, resume_dir):
+    if os.path.exists(save_dir):
+        assert save_dir != resume_dir
+        decision = input("\nDirectory {} already exists. Overwrite? [y/n]: ".format(save_dir))
+        while True:
+            if decision == "y":
+                shutil.rmtree(save_dir)
+                os.mkdir(save_dir)
+                break
+            elif decision == "n":
+                raise Exception
+    else:
+        os.mkdir(save_dir)
+
+def print_sample(sample, code_to_chars, code_to_arpa):
+    print("\nSAMPLE OF DATA\n" + "="*20 + "\n")
+
+    X, Y_in, Y_targ, len_X, len_Y = sample
+    X = convert_list(X.T, code_to_chars)
+    Y_in = convert_list(Y_in.T, code_to_arpa)
+    Y_targ = convert_list(Y_targ.T, code_to_arpa)
+    for i in range(len(X)):
+        print("Example {}".format(i + 1))
+        print("\tX: " + str(X[i]))
+        print("\tY: " + str(Y_in[i]))
+        print("\tY: " + str(Y_targ[i]))
+        print("\tlen_x: " + str(len_X[i]))
+        print("\tlen_y: " + str(len_Y[i]))
+        print()
+        print()
+    print()
+
+def print_prediction(X, prediction, code_to_chars, code_to_arpa):
+    zipped = zip(X.T, prediction.T)
+    for s, p in zipped:
+        spelling_raw = convert_word(s, code_to_chars)
+        spelling = "".join([ch for ch in spelling_raw if "<" not in ch])
+        arpa_raw = convert_word(p, code_to_arpa)
+        arpa = " ".join([ar for ar in arpa_raw if "<" not in ar])
+        print("{} - {}".format(spelling, arpa))
+
+def evaluate(target, prediction):
+    zipped = zip(target, prediction)
+    for a, b in zipped:
+        print(a)
+
+CharToPhonModel().train()
