7549a3abc9a9d24f91cada19f28f76ec85e98f91
randomsgs <brandenchan@icloud.com>
2018-10-28 01:21:11 +0100

    Commenting down to the inference_one function

--- model.py
+++ model.py
@@ -1,47 +1,52 @@
+""" Contains the CharToPhonModel class which handles the construction
+of a character level sequence to sequence model that predicts pronunciation 
+(ARPA format) from spelling """
+
 import os
+import pickle
 import shutil
 from pprint import pprint
-import pickle
 
+import numpy as np
 import tensorflow as tf
+
+from data_handling import (convert_list, convert_word,
+                           joint_iterator_from_file, load_maps, load_symbols)
+from evaluation import dev_stats, evaluate
+from graph import create_graph
+
 try:
     from tensorflow.nn.rnn_cell import LSTMCell, LSTMStateTuple, DropoutWrapper
 except ModuleNotFoundError:
     from tensorflow.contrib.rnn import LSTMCell, LSTMStateTuple, DropoutWrapper
-import numpy as np
 
-
-from data_handling import (convert_list, joint_iterator_from_file, load_maps,
-                           load_symbols, convert_word)
-from evaluation import dev_stats, evaluate
-from graph import create_graph
 
 PAD_CODE = 0
 START_CODE = 1
 END_CODE = 2
 
 class CharToPhonModel:
-    """ Class that handles training and inference of LSTM model """
+    """ Class that handles training and inference of sequence to sequence model"""
     def __init__(self,
                 data_dir="data/",
                 batch_size=128,
                 embed_dims=300,
                 hidden_dims=300,
                 bidir=True,
-                cell_class=LSTMCell,
-                max_gradient_norm=1,
+                cell_class="lstm",
+                max_gradient_norm=1,    # for gradient clipping
                 learning_rate=0.001,
                 save_dir="unsaved_model/",
                 resume_dir=None,
-                n_batches=10001,
-                debug=False,
+                n_batches=10001,    
+                debug=False,        # uses smaller datasets if true
                 print_every=50,
-                validate_every=500,
-                initializer=tf.glorot_normal_initializer,
-                attention=tf.contrib.seq2seq.LuongAttention,
-                dropout=0.0,
-                anneal_steps=1000,
-                anneal_decay=0.95
+                save_every=500,
+                initializer="glorot",
+                attention="luong",
+                dropout=0.0,        # 0. is no dropout and 1. is full dropout
+                anneal_steps=1000,   # annealing happens after this many steps
+                anneal_decay=0.95   # learning rate is annealed by this factor
                 ):
 
         self.data_dir = data_dir
@@ -53,7 +58,6 @@ class CharToPhonModel:
         self.embed_dims = embed_dims
         self.hidden_dims = hidden_dims
         self.bidir = bidir
-        self.cell_class=LSTMCell
         self.max_gradient_norm = max_gradient_norm
         self.learning_rate = learning_rate
         self.save_dir = save_dir
@@ -61,9 +65,9 @@ class CharToPhonModel:
         self.n_batches = n_batches
         self.debug = debug
         self.print_every = print_every
-        self.save_every = validate_every
+        self.save_every = save_every
+        self.sample_every = save_every
         self.initializer = initializer
-        self.attention = attention     
         self.dropout = dropout
         self.anneal_steps = anneal_steps
         self.anneal_decay = anneal_decay
@@ -71,9 +75,21 @@ class CharToPhonModel:
         sample_file = self.data_dir + "sample"
         self.iter_sample = joint_iterator_from_file(sample_file, auto_reset=False)
 
+        if cell_class.lower() == "lstm":
+            self.cell_class = LSTMCell
+
+        if attention.lower() == "luong":
+            self.attention = tf.contrib.seq2seq.LuongAttention     
+
+        if initializer.lower() == "glorot":
+            self.initializer = tf.glorot_normal_initializer
 
     def build_graph(self):
+        """ Handles the construction of the tf computational graph. Returns
+        placeholders and the output_nodes which are dictionaries of tensors"""
+
         tf.reset_default_graph()
+
         tf.get_variable_scope().set_initializer(self.initializer())
 
         placeholders = self.setup_placeholders()
@@ -89,47 +105,51 @@ class CharToPhonModel:
                                                         placeholders["encoder_input_lengths"])
 
         if self.mode == "inference":
-
             output_nodes = {"predictions_arpa": predictions_arpa}
 
         if self.mode == "train":
-
             losses, batch_loss = self.compute_loss(logits,
                                                     placeholders["decoder_targets"],
                                                     placeholders["decoder_lengths"])
             train_op = self.gradient_update(batch_loss)
             output_nodes = {"train_op": train_op, 
-                            "batch_loss": batch_loss, 
+                            "batch_loss": batch_loss,
                             "losses": losses,
                             "predictions_arpa": predictions_arpa}
 
         return placeholders, output_nodes
 
-
     def setup_placeholders(self):
-        with tf.variable_scope("variables"):
+        """Initializes all necessary placeholders.
+        Returns them in a dictionary"""
 
-            # inputs of shape [encoder_max_time, batch_size]
+        with tf.variable_scope("variables"):
+            
+            # Sequence where each int represents an orthogrpahic character
+            # of the input word (shape: [encoder_max_time, batch_size])
             encoder_inputs = tf.placeholder(shape=(None, None),
                                                 dtype=tf.int32,
                                                 name="encoder_inputs")
 
-            # inputs of shape [decoder_max_time, batch_size]
+            # Sequence where each int represents an ARPA character
+            # of the output pronunciation (shape: [decoder_max_time, batch_size]) 
             decoder_inputs = tf.placeholder(shape=(None, None),
                                                 dtype=tf.int32,
                                                 name="decoder_inputs")
 
-            # targets of shape [decoder_max_time - 1, batch_size]
+            # Sequence where each int represents an ARPA character
+            # of the output pronunciation but without the start token
+            # (shape: [decoder_max_time - 1, batch_size])
             decoder_targets = tf.placeholder(shape=(None, None),
                                                 dtype=tf.int32,
                                                 name="decoder_targets")
 
-            # sequence lengths of shape [batch_size]
+            # Length of each orthographic input word (shape: [batch_size])
             encoder_input_lengths = tf.placeholder(shape=(None,),
                                                         dtype=tf.int32,
                                                         name='encoder_inputs_lengths')
 
-            # sequence lengths of shape [batch_size]
+            # Length of each ARPA pronunciation (shape: [batch_size])
             decoder_lengths = tf.placeholder(shape=(None,),
                                              dtype=tf.int32,
                                              name='decoder_lengths')
@@ -143,38 +163,41 @@ class CharToPhonModel:
 
 
     def build_encoder(self, encoder_inputs, encoder_input_lengths):
+        """ Builds an RNN encoder. Can be configured to be uni- / bi- directional.
+        Can also enable dropout. Returns outputs of the RNN at each timestep and 
+        also the final state """
 
         with tf.variable_scope("encoder"):
 
+            # Embeddings for orthographic characters
             char_embeddings = tf.Variable(tf.random_uniform((self.n_chars, self.embed_dims), -1.0, 1.0),
                                             name="char_embeddings")
-
             encoder_input_embeddings = tf.nn.embedding_lookup(char_embeddings, encoder_inputs)
 
             # Unidirectional Run
             if not self.bidir:
                 encoder_cell = self.cell_class(self.hidden_dims)
-                if self.mode != "inference":
+                if self.mode == "training":
                     encoder_cell = DropoutWrapper(encoder_cell,
                                                     input_keep_prob=1.0-self.dropout,
                                                     output_keep_prob=1.0-self.dropout,
                                                     state_keep_prob=1.0-self.dropout)
                 encoder_outputs, encoder_final_state = tf.nn.dynamic_rnn(
-                                            encoder_cell, encoder_input_embeddings,
-                                            dtype=tf.float32, time_major=True)
+                                                        encoder_cell, encoder_input_embeddings,
+                                                        dtype=tf.float32, time_major=True)
 
             # Bidirectional Run
             else:
                 with tf.variable_scope("fw"):
                     fw_encoder_cell = self.cell_class(self.hidden_dims)
-                    if self.mode != "inference":
+                    if self.mode == "training":
                         fw_encoder_cell = DropoutWrapper(fw_encoder_cell,
                                                         input_keep_prob=1.0-self.dropout,
                                                         output_keep_prob=1.0-self.dropout,
                                                         state_keep_prob=1.0-self.dropout)
                 with tf.variable_scope("bw"):
                     bw_encoder_cell = self.cell_class(self.hidden_dims)
-                    if self.mode != "inference":
+                    if self.mode == "training":
                         bw_encoder_cell = DropoutWrapper(bw_encoder_cell,
                                                         input_keep_prob=1.0-self.dropout,
                                                         output_keep_prob=1.0-self.dropout,
@@ -190,15 +213,12 @@ class CharToPhonModel:
                 # Concat final states of forward and backward run
                 encoder_final_state_c = tf.concat(
                     (encoder_fw_final_state.c, encoder_bw_final_state.c), 1)
-
                 encoder_final_state_h = tf.concat(
                     (encoder_fw_final_state.h, encoder_bw_final_state.h), 1)
-
                 encoder_final_state = LSTMStateTuple(
                     c=encoder_final_state_c,
                     h=encoder_final_state_h
                 )
-
                 encoder_outputs = tf.concat((encoder_fw_outputs, encoder_bw_outputs), -1)
 
             return encoder_outputs, encoder_final_state
@@ -207,25 +227,33 @@ class CharToPhonModel:
     def build_decoder(self, encoder_outputs, encoder_final_state,
                       decoder_inputs, decoder_targets,
                       decoder_lengths, encoder_input_lengths):
+        """Builds an RNN decoder.
+        Can also use dropout and an attention mechanism."""
 
         with tf.variable_scope("decoder"):
 
+            # Embeddings for arpa phonetic characters
             arpa_embeddings = tf.Variable(tf.random_uniform((self.n_arpa, self.embed_dims), -1.0, 1.0),
                                             name="arpa_embeddings")
-
             decoder_input_embeddings = tf.nn.embedding_lookup(arpa_embeddings, decoder_inputs)
 
+            # Dense layer that each timestep output is sent to
             with tf.variable_scope("projection"):
                 projection_layer = tf.layers.Dense(
                                             self.n_arpa, use_bias=False)
 
-
-            # Cell definition
+            # Cell definition with dropout for training
             decoder_dims = self.hidden_dims
             if self.bidir:
                 decoder_dims *= 2
             decoder_cell = self.cell_class(decoder_dims)
+            if self.mode == "training":
+                decoder_cell = DropoutWrapper(decoder_cell,
+                                                input_keep_prob=1.0-self.dropout,
+                                                output_keep_prob=1.0-self.dropout,
+                                                state_keep_prob=1.0-self.dropout)
 
+            # Attention wrapper
             if self.attention is not None:
                 attention_states = tf.transpose(encoder_outputs, [1, 0, 2])
                 attention_mechanism = self.attention(
@@ -236,8 +264,6 @@ class CharToPhonModel:
                                     decoder_cell, attention_mechanism,
                                     attention_layer_size=decoder_dims)
 
-
-
             # Define decoder initial state
             if self.attention is not None:
                 decoder_initial_state = decoder_cell.zero_state(self.batch_size, tf.float32).clone(
@@ -247,14 +273,15 @@ class CharToPhonModel:
 
 
             # Define helper
+            # Input at each timestep is label arpa sequence
             if self.mode == "train":
                 helper = tf.contrib.seq2seq.TrainingHelper(
                                 inputs=decoder_input_embeddings, 
                                 sequence_length=decoder_lengths,
                                 time_major=True)
+            # Inference argmax predictions are inputs to the next timestep
             elif self.mode == "inference":
                 start_tokens = tf.fill([self.batch_size], START_CODE)
-
                 helper = tf.contrib.seq2seq.GreedyEmbeddingHelper(
                             arpa_embeddings,
                             start_tokens,
@@ -266,14 +293,15 @@ class CharToPhonModel:
                             decoder_initial_state,
                             output_layer=projection_layer)
 
+            # Inference predictions are limited to 2 times the input sequence length
             maximum_iterations = tf.round(tf.reduce_max(encoder_input_lengths) * 2)
 
+            # Decoding loop output
             outputs, _, _ = tf.contrib.seq2seq.dynamic_decode(
                                                     my_decoder,
                                                     output_time_major=True,
                                                     impute_finished=True,
                                                     maximum_iterations=maximum_iterations)
-
             logits = outputs.rnn_output
 
             # Transposed so that not time major
@@ -283,6 +311,8 @@ class CharToPhonModel:
 
 
     def compute_loss(self, logits, decoder_targets, decoder_lengths):
+        """ Return losses which is the masked per example cross entropy loss. Also
+        return batch_loss which is the per batch average loss"""
 
         with tf.variable_scope("loss_computation"):
 
@@ -301,6 +331,9 @@ class CharToPhonModel:
             return losses, batch_loss
 
     def gradient_update(self, batch_loss):
+        """ Return the optimization op. Gradient clipping and annealing is applied. ADAM is
+        the default optimization regime """
+
         # Calculate and clip gradients
         params = tf.trainable_variables()
         gradients = tf.gradients(batch_loss, params)
@@ -308,7 +341,6 @@ class CharToPhonModel:
                                 gradients, self.max_gradient_norm)
 
         # Optimization
-
         global_step = tf.Variable(0, trainable=False)
         annealed_lr = tf.train.exponential_decay(self.learning_rate, global_step,
                                                 self.anneal_steps, self.anneal_decay, staircase=True)
@@ -319,12 +351,15 @@ class CharToPhonModel:
         return train_op
 
     def train(self):
+        """ Train the model """
         self.mode = "train"
         self.train_setup()
         self.train_loop()
 
     def train_setup(self):
-        
+        """ Do a save directory check. Set up train data iterator. Show what the data looks like.
+        Print out some essential model details."""
+
         check_save_dir(self.save_dir, self.resume_dir)
         self.save_hyperparams()
 
@@ -355,7 +390,8 @@ class CharToPhonModel:
                                                                               int(n_to_process / self.n_train_data)))
 
     def train_loop(self):
-
+        """ Start training loop. Optimize model per batch. Save model intermittently.
+        Print and save samples. Save per batch losses. """
         completed_batches = 0
         loss_track = []
 
@@ -385,19 +421,24 @@ class CharToPhonModel:
                         if completed_batches % self.print_every == 0:
                             t_loss = np.mean(loss_track[-100:])
                             print("Batch {} / {} Epoch {} train: {}".format(completed_batches, self.n_batches, epoch, t_loss))
-                        if completed_batches % self.save_every == 0:
+
+                        if completed_batches % self.sample_every == 0:
                             self.sample_inference("train_sample.txt", placeholders, out_nodes, completed_batches, sess)
+
+                        if completed_batches % self.save_every == 0:
                             save_path = saver.save(sess, self.save_dir + "model.ckpt.{}".format(completed_batches))
                             print("Model saved in path: {}".format(save_path))
                             pickle.dump(loss_track, open(self.save_dir + "results/loss_track.pkl", "wb"))
 
 
     def inference(self):
+        """ Perform inference with a saved model. """
         ckpt_batch_idx = self.inference_setup()
         self.inference_loop(ckpt_batch_idx)
         create_graph()
 
     def inference_setup(self):
+        """ Return all the model checkpoint filenanes. Setup a dev data iterator. """
         self.mode = "inference"
         dev_file = self.data_dir + "dev"
         train_slice_file = self.data_dir + "train"
@@ -410,6 +451,8 @@ class CharToPhonModel:
         return ckpt_batch_idx
 
     def inference_loop(self, ckpt_batch_idx):
+        """ Perform inference with a collection of model checkpoints contained in ckpt_batch_idx.
+        Save sample inference in dev_sample.txt. Save model checkpoint performance in metrics.txt. """
         iterators = {"development": self.iter_dev,
                      "training": self.iter_train_slice}
         metrics_filename = self.save_dir + "results/metrics.txt"
@@ -450,6 +493,7 @@ class CharToPhonModel:
                     self.sample_inference("dev_sample.txt", placeholders, out_nodes, idx, sess)    
 
     def sample_inference(self, filename, placeholders, out_nodes, n_batches, sess):
+        """ Perform inference on a sample of the data. Write to file """
         _, sample_predictions, sample_X = self.inference_one(self.iter_sample,
                                                             placeholders,
                                                             out_nodes,
@@ -464,6 +508,7 @@ class CharToPhonModel:
                                                                 sample_predictions_format))
 
     def test(self):
+        """ Writes model performance to file. Performs inference on test set """
         self.mode = "inference"
         test_file = self.data_dir + "test"
         self.iter_test = joint_iterator_from_file(test_file, auto_reset=False)
